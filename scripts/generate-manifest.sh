abf9663a521198057b5343598bbd0a31e1399a26
Sai1999ram <129557736+Sai1999ram@users.noreply.github.com>
2025-05-20 16:45:20 -0400

    apply these changes

--- scripts/generate-manifest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-terraform state list > resources.txt
-jq -R -s -c 'split("\n") | map(select(length > 0))' resources.txt > resource-manifest.json
-aws s3 cp resource-manifest.json s3://terraform-artifacts/resource-manifest.json
