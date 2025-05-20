76d7364bb453f73f91e20da4194717792b97dd05
Sai1999ram <129557736+Sai1999ram@users.noreply.github.com>
2025-05-20 16:48:20 -0400

    deleted backend.tf for temporary

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "vubber-terraform-artifacts-v2"
-    key            = "terraform.tfstate"
-    region         = "us-west-1"
-    # dynamodb_table = "terraform-locks"  # Optional: only if you set up state locking
-  }
-}
