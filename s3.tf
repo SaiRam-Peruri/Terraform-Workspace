2843db14e82b3653a90c89fd5fe2b56acaf41feb
Sai1999ram <129557736+Sai1999ram@users.noreply.github.com>
2025-05-20 18:22:47 -0400

    updated s3 to look statefile

--- s3.tf
+++ s3.tf
@@ -10,3 +10,12 @@ resource "aws_s3_bucket" "terraform_artifacts_v2" {
     Environment = "Dev"
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-artifacts-v2"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
