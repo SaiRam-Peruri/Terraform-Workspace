76e80c335f7359d815f3d691ede6a4d54ec06c9e
Sai1999ram <129557736+Sai1999ram@users.noreply.github.com>
2025-05-20 19:26:15 -0400

    removed backend.tf as we are just creating the backend S3 bucket there

--- bootstrap/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-artifacts-v2"
-    key            = "state/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
