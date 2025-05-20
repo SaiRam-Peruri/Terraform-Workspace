1116b4eb19dd32c611fc92c231d6b4a87b3680cb
Sai1999ram <129557736+Sai1999ram@users.noreply.github.com>
2025-05-20 16:54:00 -0400

    removed lambda in api gateway

--- api-gateway.tf
+++ api-gateway.tf
@@ -23,18 +23,3 @@ resource "aws_api_gateway_method" "update_transcription" {
   http_method   = "PUT"
   authorization = "NONE"
 }
-
-# Permissions for API Gateway to Invoke Lambda
-resource "aws_lambda_permission" "api_gateway_invoke_get" {
-  statement_id  = "AllowAPIGatewayInvokeGet"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.transcription_lambda_v2.function_name
-  principal     = "apigateway.amazonaws.com"
-}
-
-resource "aws_lambda_permission" "api_gateway_invoke_put" {
-  statement_id  = "AllowAPIGatewayInvokePut"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.transcription_lambda_v2.function_name
-  principal     = "apigateway.amazonaws.com"
-}
