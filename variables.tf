ceb6542c6e19d3e66e20d601a7440050f5a130b5
Sai1999ram <129557736+Sai1999ram@users.noreply.github.com>
2025-05-20 20:41:33 -0400

    issue with AZ's in us-west-1

--- variables.tf
+++ variables.tf
@@ -15,16 +15,15 @@ variable "vpc_cidr" {
 
 variable "private_subnets" {
   default = {
-    "terraform-private-subnet-1" = 1
-    "terraform-private-subnet-2" = 2
-    "terraform-private-subnet-3" = 3
+    "terraform-private-subnet-1" = 0
+    "terraform-private-subnet-2" = 1
   }
 }
 
 variable "public_subnets" {
   default = {
-    "terraform-public-subnet-1" = 1
-    "terraform-public-subnet-2" = 2
-    "terraform-public-subnet-3" = 3
+    "terraform-public-subnet-1" = 0
+    "terraform-public-subnet-2" = 1
   }
 }
+
