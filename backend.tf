b7d9c410a0f469c847d00c700540a0b9c1dc84cb
Sai1999ram <129557736+Sai1999ram@users.noreply.github.com>
2025-05-21 22:09:05 -0400

    changed region

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "terraform-states-sairam-2025"
     key     = "state/terraform.tfstate"
-    region  = "us-west-1"
+    region  = "us-east-1"
     encrypt = true
   }
 }
